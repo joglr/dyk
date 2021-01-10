44e9ee2dedf2944b5a34c51f7b728082abc9d816
Jonas Røssum <jonas.g.roessum@gmail.com>
2021-01-10 13:32:45 +0100

    Enable concurrent mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = document.getElementById("root") as HTMLElement;
+
+// @ts-ignore
+ReactDOM.unstable_createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
